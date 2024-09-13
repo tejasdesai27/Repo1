aa1e97a6c1c107bae766e5e7ef4c7a0098b1d42b
tejasdesai27 <139948226+tejasdesai27@users.noreply.github.com>
2024-09-13 03:05:50 +0000

    ADDED TEST CASES

--- HW2/test_cases.py
+++ HW2/test_cases.py
@@ -1,15 +1,15 @@
 from hw2_debugging import merge_sort
 
 
-# pass  
+# pass1
 def test_case1():     
     assert merge_sort([1,2,6,4]) == [1,2,4,6]     
 
-# pass 
+# pass2 
 def test_case2():
     assert merge_sort([8,4,7,1]) == [1,4,7,8]  
 
-# pass        
+# pass3        
 def test_case3():
     assert merge_sort([]) == []   
     
